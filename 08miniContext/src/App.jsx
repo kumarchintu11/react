e87384399b7a679a08d7e66ae2d12bd1873ec09e
kumarchintu11 <159183397+kumarchintu11@users.noreply.github.com>
2024-08-21 17:59:07 +0530

    context API project discussion

---
+++ 08miniContext/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './App.css'
+import UserContextProvider from './Context/userContextProvider'
+import Login from './Components/Login'
+import Profile from './Components/Profile'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <UserContextProvider>
+      <h1>Context API</h1>
+      <Login />
+      <Profile />
+    </UserContextProvider>
+  )
+}
+
+export default App
---
+++ 08miniContext/src/Context/userContext.js
@@ -0,0 +1,9 @@
+// Context file cotains pure javascript file.
+//1. create context and export it
+//2. Create a provider(it will provide the data)
+
+
+import React from "react";
+const UserContext =React.createContext()
+
+export default UserContext;
